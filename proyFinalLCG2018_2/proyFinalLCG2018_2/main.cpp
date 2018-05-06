5c7a08a425224e5b19a799cd92d7f4914741402e
Svare <jepf_27@hotmail.es>
2018-05-06 00:29:23 -0500

    Se añaden las funciones skyBox y plane para hacer el cielo y planos, se añade una caja con la entrada y se empieza a trabajar en el estacionamiento, tambien se empezo a trabajar con la camara y las animaciones.

--- proyFinalLCG2018_2/proyFinalLCG2018_2/main.cpp
+++ proyFinalLCG2018_2/proyFinalLCG2018_2/main.cpp
@@ -22,27 +22,73 @@
 #include "Main.h"
 #include "renders.h"
 #include "texture.h"
+#include "Camera.h"
 
 /* VARIABLES GLOBALES */
 
-float rotX = 0.0f, rotY = 0.0f;
+float movX = 0.0f, movY = 0.0f, movZ = 0.0f;
+float sideSkyBox = 0.0f;
+
+/* RENDERS */
 
 Render renders;
 
 /* TEXTURAS */
 
+CTexture none;
 CTexture wood;
+CTexture skyBox;
+CTexture tabiqueMarron;
+CTexture entradaTabiqueMarron;
+CTexture grass;
+CTexture asfalto;
+
+/* CAMARA */
+
+CCamera camera;
+GLfloat g_lookupdown = 0.0f;    // Look Position In The Z-Axis (NEW) 
+
 
 void InitGL (GLvoid) {
+
 	 glClearDepth(1.0f);		// Activamos el valor de inicio del buffer de profundidad
 	 glEnable(GL_DEPTH_TEST);	// Hacemos la prueba de profundidad
 	 glDepthFunc(GL_LEQUAL);	// Tipo de prueba de profundidad a hacer
 
 	 /* TEXTURAS */
 
+	 none.LoadTGA("texturas/none.tga");
+	 none.BuildGLTexture();
+	 none.ReleaseImage();
+	 
 	 wood.LoadTGA("texturas/wood.tga");
 	 wood.BuildGLTexture();
 	 wood.ReleaseImage();
+
+	 skyBox.LoadTGA("texturas/skyBox.tga");
+	 skyBox.BuildGLTexture();
+	 skyBox.ReleaseImage();
+
+	 tabiqueMarron.LoadTGA("texturas/tabiqueMarron.tga");
+	 tabiqueMarron.BuildGLTexture();
+	 tabiqueMarron.ReleaseImage();
+
+	 entradaTabiqueMarron.LoadTGA("texturas/entradaTabiqueMarron.tga");
+	 entradaTabiqueMarron.BuildGLTexture();
+	 entradaTabiqueMarron.ReleaseImage();
+
+	 grass.LoadTGA("texturas/grass.tga");
+	 grass.BuildGLTexture();
+	 grass.ReleaseImage();
+
+	 asfalto.LoadTGA("texturas/asfalto.tga");
+	 asfalto.BuildGLTexture();
+	 asfalto.ReleaseImage();
+
+	 /* CAMARA */
+
+	 camera.Position_Camera(0.0f, 2.5f, 3.0f, 0.0f, 2.5f, 0.0f, 0.0f, 1.0f, 0.0f);
+
 
 	 // glEnable(GL_TEXTURE_2D);
 
@@ -72,7 +118,15 @@ void display(void) {
 
 	////////// AREA DE DIBUJO //////////////////////////////////////////////////////////////////////////////////////
 
-	glTranslatef(0.0, 0.0, -20.0); // Trasladamos en Z para que se vea todo lo que viene.
+	// CAMARA //
+
+	glRotatef(g_lookupdown, 1.0f, 0.0f, 0.0f); // Mover camara arriba/abajo
+
+	gluLookAt(camera.mPos.x, camera.mPos.y, camera.mPos.z,
+				camera.mView.x, camera.mView.y, camera.mView.z,
+				camera.mUp.x, camera.mUp.y, camera.mUp.z);
+
+	glTranslatef(0.0 + movX, 0.0 + movY, -20.0 + movZ); // Trasladamos en Z para que se vea todo lo que viene.
 
 	// EJES DE REFERENCIA
 
@@ -91,14 +145,31 @@ void display(void) {
 	// FIN EJES DE REFERENCIA
 
 	glColor3f(1.0, 1.0, 1.0);
-	//glutWireSphere(1.0f, 30, 30);
-	
-	glRotatef(rotX, 1.0, 0.0, 0.0);
-	glRotatef(rotY, 0.0, 1.0, 0.0);
 
 	glEnable(GL_TEXTURE_2D);
-	renders.cube(10.0, 10.0, 10.0, wood.GLindex, wood.GLindex, wood.GLindex, wood.GLindex, wood.GLindex, wood.GLindex);
+	
+	
+	renders.skyBox(50.0, 20.0, 80.0, skyBox.GLindex, grass.GLindex, skyBox.GLindex,sideSkyBox);
+
+	glPushMatrix(); // Entrada
+		glEnable(GL_ALPHA_TEST);
+		glAlphaFunc(GL_GREATER, 0.1);
+
+		glTranslatef(0.0f, -8.75f, 0.0f);
+		renders.cube(5.0, 2.5, 3.0,
+						tabiqueMarron.GLindex, none.GLindex, tabiqueMarron.GLindex,
+						tabiqueMarron.GLindex, entradaTabiqueMarron.GLindex, entradaTabiqueMarron.GLindex);
+
+		glDisable(GL_ALPHA_TEST);
+	glPopMatrix(); // Fin Entrada
+
+	glTranslatef(0.0f, -9.99f, 30.0f);
+	glRotatef(90.0, -1.0f, 0.0f, 0.0f);
+	renders.plane(50.0, 20.0,0, asfalto.GLindex);
+
 	glDisable(GL_TEXTURE_2D);
+	
+	
 
 	glFlush();
 	glutSwapBuffers ( );
@@ -108,18 +179,66 @@ void display(void) {
 void keyboard(unsigned char key, int x, int y) {
 	switch(key) {
 		case 27: exit(0);
-		break;
+			break;
+		case 'w':   //Movimientos de camara
+		case 'W':
+			camera.Move_Camera(CAMERASPEED + 0.2);
+			break;
+
+		case 's':
+		case 'S':
+			camera.Move_Camera(-(CAMERASPEED + 0.2));
+			break;
+
+		case 'a':
+		case 'A':
+			camera.Strafe_Camera(-(CAMERASPEED + 0.4));
+			break;
+
+		case 'd':
+		case 'D':
+			camera.Strafe_Camera(CAMERASPEED + 0.4);
+			break;
+
+		case 'q':
+		case 'Q':
+			camera.UpDown_Camera(CAMERASPEED + 0.02);
+			break;
+
+		case 'e':
+		case 'E':
+			camera.UpDown_Camera(-(CAMERASPEED + 0.02));
+			break;
 
 		case 'x':
-		break;
+			movX += 1.0;
+			break;
+		case 'y':
+			movY += 1.0;
+			break;
+		case 'z':
+			movZ += 1.0;
+			break;
+		case 'X':
+			movX -= 1.0;
+			break;
+		case 'Y':
+			movY -= 1.0;
+			break;
+		case 'Z':
+			movZ -= 1.0;
+			break;
 	}
+
 	glutPostRedisplay();
 	return;
+
 }  
 
 void specialKeys(int key, int x, int y) {
+
 	switch(key) {
-		case GLUT_KEY_UP:
+		/*case GLUT_KEY_UP:
 			rotX += 2.0f;
 			break;
 		case GLUT_KEY_DOWN:
@@ -132,8 +251,48 @@ void specialKeys(int key, int x, int y) {
 			rotY -= 2.0f;
 			break;
 		default:
+			break;*/
+		case GLUT_KEY_PAGE_UP:
+			camera.UpDown_Camera(CAMERASPEED);
+			break;
+
+		case GLUT_KEY_PAGE_DOWN:
+			camera.UpDown_Camera(-CAMERASPEED);
+			break;
+
+		case GLUT_KEY_UP:     // Presionamos tecla ARRIBA...
+			g_lookupdown -= 1.0f;
+			break;
+
+		case GLUT_KEY_DOWN:               // Presionamos tecla ABAJO...
+			g_lookupdown += 1.0f;
+			break;
+
+		case GLUT_KEY_LEFT:
+			camera.Rotate_View(-CAMERASPEED);
+			break;
+
+		case GLUT_KEY_RIGHT:
+			camera.Rotate_View(CAMERASPEED);
+			break;
+
+		default:
 			break;
 	}
+
+	glutPostRedisplay();
+
+}
+
+void animation() {
+
+	/* ANIMACION SKYBOX --> MOVIMIENTO DEL CIELO */
+
+	if (sideSkyBox >= 1.0)
+		sideSkyBox = 0.0;
+	else
+		sideSkyBox += 0.00001;
+
 	glutPostRedisplay();
 }
 
@@ -149,10 +308,11 @@ int main(int argc, char **argv) {
 
 	InitGL(); // Inicializacion del sistema.
 
-	glutDisplayFunc(display); 
-	glutReshapeFunc(reshape);
-	glutKeyboardFunc(keyboard);
-	glutSpecialFunc(specialKeys);
+	glutDisplayFunc		( display ); 
+	glutReshapeFunc		( reshape );
+	glutKeyboardFunc	( keyboard );
+	glutSpecialFunc		( specialKeys );
+	glutIdleFunc		( animation );
 	glutMainLoop();
 	 
 	return 0; // ANSI C requiere que main retorne un valor entero.
--- proyFinalLCG2018_2/proyFinalLCG2018_2/renders.h
+++ proyFinalLCG2018_2/proyFinalLCG2018_2/renders.h
@@ -6,4 +6,6 @@ class Render {
 	
 		// void skyBox(float lenght, float height, float width, GLuint sideTexture);
 		void cube(float lenght, float height, float width, GLuint top, GLuint down, GLuint left, GLuint right, GLuint front, GLuint back);
+		void skyBox(float lenght, float height, float width, GLuint top, GLuint down, GLuint side, float sideMov);
+		void plane(float lenght, float height, int repeat ,GLuint texture);
 };
